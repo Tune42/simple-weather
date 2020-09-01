44472710a204c8d126030d9628aeaa742a92627f
Tune42 <59482059+Tune42@users.noreply.github.com>
2020-09-01 16:59:10 -0500

    Updates

---
+++ precache-manifest.cec73405ecd65188f98785f731e5a928.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7e54eace019da6116ccc2150177d14",
+    "url": "/simple-weather/index.html"
+  },
+  {
+    "revision": "04a0efd3bc4c95f3d65a",
+    "url": "/simple-weather/static/css/2.3807f13a.chunk.css"
+  },
+  {
+    "revision": "784b7abf359acaaf21ad",
+    "url": "/simple-weather/static/css/main.7cfcf69b.chunk.css"
+  },
+  {
+    "revision": "04a0efd3bc4c95f3d65a",
+    "url": "/simple-weather/static/js/2.3b17f101.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-weather/static/js/2.3b17f101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784b7abf359acaaf21ad",
+    "url": "/simple-weather/static/js/main.266dbeea.chunk.js"
+  },
+  {
+    "revision": "99039606ee1ac868bf01",
+    "url": "/simple-weather/static/js/runtime-main.4b74cb1c.js"
+  },
+  {
+    "revision": "b416dbc329e9f5a7745711757b6bf072",
+    "url": "/simple-weather/static/media/background.b416dbc3.jpg"
+  }
+]);
